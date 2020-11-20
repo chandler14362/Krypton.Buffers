4642bb50a05ced7c11077e333e3f4d3ab2ca4fb0
Chandler Stowell <chandler14362@gmail.com>
2020-11-19 23:17:32 -0500

    new pooling strategies for buffer resizing

---
+++ Krypton.Buffers/IPoolingStrategy.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Krypton.Buffers
+{
+    public interface IPoolingStrategy
+    {
+        /// <summary>
+        /// Called whenever a buffer writer needs to resize
+        /// </summary>
+        /// <param name="size">The current size of the buffer</param>
+        /// <param name="neededSize">The needed size of the buffer</param>
+        /// <returns>The resized buffer</returns>
+        Memory<byte> Resize(int size, int neededSize);
+
+        /// <summary>
+        /// Called whenever a buffer writer needs to free pooled data
+        /// </summary>
+        /// <param name="data">The pooled data</param>
+        void Free(Memory<byte> data);
+    }
+}
